a9e3fbd5f0ade93c0d46f211dbcb238fe47e67b2
kdurnd <2806881473@qq.com>
2024-07-11 00:07:27 +0800

    以屎山的方式勉强完成切换版本的按钮的动效

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -18,12 +18,27 @@
     <div class="filter">
       <span>{{ mode }}</span>
       <div class="track">
-        <doubleThumb style="height: 40px;" :min=0 :max=22 :step=1 :describe="describeByLevel" @filter="filterByLevel"></doubleThumb>
+        <doubleThumb style="height: 40px;" :min=0 :max=22 :step=1 :describe="describeByLevel" @filter="filterByLevel">
+        </doubleThumb>
       </div>
       <label class="chooseMode">
         <checkbox size="width: 32px;height: 32px;" v-model="chooseMode"></checkbox>
         <span>使用定数筛选</span>
       </label>
+    </div>
+    <div class="switchVersion">
+      <div class="switchItem active" @click="toOld">
+        旧乐谱
+      </div>
+      <div class="switchItem" @click="toNew">
+        DX 2024
+      </div>
+      <div class="switchItemLine"></div>
+    </div>
+    <div class="tableContainer">
+      <div class="oldTable">
+      </div>
+      <div class="newTable"></div>
     </div>
   </div>
 </template>
@@ -31,7 +46,7 @@
 <script setup>
 import checkbox from './components/checkbox.vue'
 import doubleThumb from './components/doubleThumb.vue'
-import { ref, watchEffect, computed } from 'vue'
+import { ref, watchEffect, computed, onMounted, nextTick } from 'vue'
 const searchMusic = ref('')
 const highLevelSet = ref(false)
 const chooseMode = ref(false)
@@ -43,8 +58,153 @@ const describeByLevel = (e) => {
   return arr[e]
 }
 const filterByLevel = (index1, index2) => {
-  console.log(index1, index2)
+  // console.log(index1, index2)
 }
+const doms = {}
+
+onMounted(() => {
+  doms.switchItem = document.querySelectorAll('.switchItem')
+  doms.switchItemLine = document.querySelector('.switchItemLine')
+
+  doms.switchItem[0].addEventListener('mousedown',async (e) => {
+    let couldDestroy = false;
+    console.log(couldDestroy)
+    const left = e.clientX - doms.switchItem[0].getBoundingClientRect().left;
+    const top = e.clientY - doms.switchItem[0].getBoundingClientRect().top;
+    const deeperBack = document.createElement('div');
+    deeperBack.style.position = 'absolute';
+    deeperBack.style.width = '100px';
+    deeperBack.style.height = '100px';
+    deeperBack.style.borderRadius = '50%';
+    deeperBack.style.transform = `translate(-50%,-50%) scale(0)`;
+    deeperBack.style.transition = '0.5s';
+    deeperBack.style.left = left + 'px';
+    deeperBack.style.top = top + 'px';
+    deeperBack.style.backgroundColor = deeperBackColor[0];
+    doms.switchItem[0].appendChild(deeperBack);
+    //强行渲染
+    deeperBack.getBoundingClientRect();
+    deeperBack.style.transform = `translate(-50%,-50%) scale(2.8)`;
+    
+    const removeMouseUpAndMouseLeaveListeners = () => {  
+        doms.switchItem[0].removeEventListener('mouseup', handleMouseUp);  
+        doms.switchItem[0].removeEventListener('mouseleave', handleMouseLeave);  
+        console.log('removeEventListener');  
+    };  
+    const handleMouseUp = () => {  
+        if (couldDestroy) {  
+            console.log('destroy in mouseup');  
+            doms.switchItem[0].removeChild(deeperBack);  
+            removeMouseUpAndMouseLeaveListeners();  
+        } else {  
+            couldDestroy = true;  
+        }  
+    };  
+
+    const handleMouseLeave = () => {  
+        if (couldDestroy) {  
+            console.log('destroy in mouseleave');  
+            doms.switchItem[0].removeChild(deeperBack);  
+            removeMouseUpAndMouseLeaveListeners();  
+        } else {  
+            couldDestroy = true;  
+        }  
+    };  
+    
+    deeperBack.addEventListener('transitionend', () => {
+      console.log('transitionend')
+      if (couldDestroy) {
+        console.log('destroy in transitionend')
+        doms.switchItem[0].removeChild(deeperBack);
+        removeMouseUpAndMouseLeaveListeners();  
+      } else {
+        couldDestroy = true;
+      }
+    })
+    doms.switchItem[0].addEventListener('mouseup', handleMouseUp);  
+    doms.switchItem[0].addEventListener('mouseleave', handleMouseLeave);  
+  })
+
+  doms.switchItem[1].addEventListener('mousedown',async (e) => {
+    let couldDestroy = false;
+    console.log(couldDestroy)
+    const left = e.clientX - doms.switchItem[1].getBoundingClientRect().left;
+    const top = e.clientY - doms.switchItem[1].getBoundingClientRect().top;
+    const deeperBack = document.createElement('div');
+    deeperBack.style.position = 'absolute';
+    deeperBack.style.width = '100px';
+    deeperBack.style.height = '100px';
+    deeperBack.style.borderRadius = '50%';
+    deeperBack.style.transform = `translate(-50%,-50%) scale(0)`;
+    deeperBack.style.transition = '0.5s';
+    deeperBack.style.left = left + 'px';
+    deeperBack.style.top = top + 'px';
+    deeperBack.style.backgroundColor = deeperBackColor[1];
+    doms.switchItem[1].appendChild(deeperBack);
+    //强行渲染
+    deeperBack.getBoundingClientRect();
+    deeperBack.style.transform = `translate(-50%,-50%) scale(2.8)`;
+    
+    const removeMouseUpAndMouseLeaveListeners = () => {  
+        doms.switchItem[1].removeEventListener('mouseup', handleMouseUp);  
+        doms.switchItem[1].removeEventListener('mouseleave', handleMouseLeave);  
+        console.log('removeEventListener');  
+    };  
+    const handleMouseUp = () => {  
+        if (couldDestroy) {  
+            console.log('destroy in mouseup');  
+            doms.switchItem[1].removeChild(deeperBack);  
+            removeMouseUpAndMouseLeaveListeners();  
+        } else {  
+            couldDestroy = true;  
+        }  
+    };  
+
+    const handleMouseLeave = () => {  
+        if (couldDestroy) {  
+            console.log('destroy in mouseleave');  
+            doms.switchItem[1].removeChild(deeperBack);  
+            removeMouseUpAndMouseLeaveListeners();  
+        } else {  
+            couldDestroy = true;  
+        }  
+    };  
+    
+    deeperBack.addEventListener('transitionend', () => {
+      console.log('transitionend')
+      if (couldDestroy) {
+        console.log('destroy in transitionend')
+        doms.switchItem[1].removeChild(deeperBack);
+        removeMouseUpAndMouseLeaveListeners();  
+      } else {
+        couldDestroy = true;
+      }
+    })
+    doms.switchItem[1].addEventListener('mouseup', handleMouseUp);  
+    doms.switchItem[1].addEventListener('mouseleave', handleMouseLeave);  
+  })
+ 
+  toOld()
+  
+})
+let deeperBackColor = ["", ""]
+const toOld = () => {
+  doms.switchItem[0].classList.add('active')
+  doms.switchItem[1].classList.remove('active')
+  doms.switchItemLine.classList.remove('right')
+  deeperBackColor[0] = 'rgba(25, 118, 210,0.3)'
+  deeperBackColor[1] = 'rgba(0,0,0,0.3)'
+}
+const toNew = () => {
+  doms.switchItem[1].classList.add('active')
+  doms.switchItem[0].classList.remove('active')
+  doms.switchItemLine.classList.add('right')
+  deeperBackColor[1] = 'rgba(25, 118, 210,0.3)'
+  deeperBackColor[0] = 'rgba(0,0,0,0.3)'
+
+}
+
+
 </script>
 
 <style scoped>
@@ -112,6 +272,8 @@ const filterByLevel = (index1, index2) => {
 }
 
 .searchMusic-placeholder {
+  pointer-events: none;
+  user-select: none;
   color: rgba(0, 0, 0, 0.6);
   font-size: 16px;
   position: absolute;
@@ -151,28 +313,13 @@ const filterByLevel = (index1, index2) => {
   height: 40px;
 }
 
-input[type="range"] {
-  width: 100%;
-  height: 2px;
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-}
+
 
 .track {
   flex: 1;
   position: relative;
 }
 
-input[type="range"]::-webkit-slider-thumb {
-  background: #008cff;
-  /* Chrome, Safari, Opera, Edge, IE */
-}
-
-input[type="range"]::-webkit-slider-runnable-track {
-  background: rgba(0, 0, 0, 1);
-  /* Chrome, Safari, Opera, Edge */
-}
 
 .chooseMode {
   line-height: 40px;
@@ -180,5 +327,74 @@ input[type="range"]::-webkit-slider-runnable-track {
   display: flex;
   align-items: center;
   user-select: none;
+}
+
+.switchVersion {
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
+  height: 50px;
+  position: relative;
+}
+
+.switchItem {
+  user-select: none;
+  position: relative;
+  width: 100px;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  color: rgba(0, 0, 0, 0.6);
+  cursor: pointer;
+  overflow: hidden;
+}
+
+.switchItem:hover {
+  background-color: rgba(0, 0, 0, 0.05);
+}
+
+.switchItem.active {
+  color: #1976D2
+}
+
+.switchItem.active:hover {
+  background-color: rgba(25, 118, 210, 0.1);
+}
+
+.switchItemLine {
+  position: absolute;
+  width: 100px;
+  height: 1px;
+  background-color: #1976D2;
+  bottom: 0;
+  left: 0;
+  transition: 0.5s;
+}
+
+.switchItemLine.right {
+  transform: translateX(100%);
+}
+
+.deeperBack {
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  top: 0;
+  left: 0;
+  z-index: -1;
+  border-radius: 50%;
+  /* transform: translate(-50%, -50%) scale(0); */
+}
+.deeperBackAnimation {
+  animation: deeper 0.5s forwards;
+}
+@keyframes deeper {
+  from {
+    transform: translate(-50%, -50%) scale(0);
+  }
+
+  to {
+    transform: translate(-50%, -50%) scale(2.8);
+  }
 }
 </style>
